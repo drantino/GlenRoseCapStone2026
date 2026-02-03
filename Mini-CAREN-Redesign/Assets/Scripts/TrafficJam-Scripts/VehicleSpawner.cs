ef59384b71ef34b6e3e47d7bc5754453b7f9af39
Braden2L <bqowl028@gmail.com>
2026-02-03 11:41:09 -0700

    Created car spawner. Fixed vehicle prefab tag from "RightShoe" to Vehicle

---
+++ Mini-CAREN-Redesign/Assets/Scripts/TrafficJam-Scripts/VehicleSpawner.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour
+{
+    public float spawnRateSec;
+    public float spawnRateVarianceSec;
+    public float currentCarsInLane = 0;
+    public float maxCarsInLane;
+
+    // Since we only have one car type/model, we only need one game object for the prefabs.
+    // TODO: Either have an array/list or a scriptable object to reference when we get many car models.
+    [SerializeField] private GameObject VehiclePrefab;
+    // TODO: Not needed but turning this into an enum would prevent errors from spelling mistakes.
+    public string footTag;
+
+    private float timeUntilNextSpawn;
+
+    // Gizmo to easily identify where the spawn point is.
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(transform.position, new Vector3(1,1,1));
+    }
+
+    void Start()
+    {
+        currentCarsInLane = 0;
+        timeUntilNextSpawn = Random.Range(spawnRateSec - spawnRateVarianceSec, spawnRateSec + spawnRateVarianceSec);
+        // Temp statement to notify of spelling mistakes.
+        if (footTag != "LeftShoe" && footTag != "RightShoe")
+        {
+            Debug.LogWarning("Variable 'footTag' was not given either 'LeftShoe' or 'RightShoe' as a value. Please correct before running the project again.");
+        }
+    }
+
+    void Update()
+    {
+        timeUntilNextSpawn -= Time.deltaTime;
+        if (timeUntilNextSpawn < 0)
+        {
+            timeUntilNextSpawn = Random.Range(spawnRateSec - spawnRateVarianceSec, spawnRateSec + spawnRateVarianceSec);
+            if (currentCarsInLane < maxCarsInLane)
+            {
+                SpawnCar();
+            }
+        }
+    }
+
+    private void SpawnCar()
+    {
+        GameObject instantiatedVehicle = Instantiate(VehiclePrefab, transform.position, transform.rotation);
+        Vehicle instantiatedVehicleScript = instantiatedVehicle.GetComponent<Vehicle>();
+        instantiatedVehicleScript.footTag = footTag;
+        currentCarsInLane++;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Vehicle")
+        {
+            Destroy(other.transform.parent.gameObject);
+            currentCarsInLane--;
+        }
+    }
+}
