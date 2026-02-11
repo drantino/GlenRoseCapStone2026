df12c2f9d72b8d8d19e6c1a28a7b65c8b7de5d85
AaronVaartstra <aaron.vaartstra@gmail.com>
2026-02-11 13:59:30 -0700

    Modified vehicle spawner values, created score keeper

---
+++ Mini-CAREN-Redesign/Assets/Scripts/TrafficJam-Scripts/TrafficJamScoreKeeper.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TrafficJamScoreKeeper : MonoBehaviour
+{
+	//private static TrafficJamScoreKeeper instance;
+	public static int rightCarsSpawned, leftCarsSpawned, rightCarsPassed, leftCarsPassed = 0;
+
+	public static void ResetValues()
+	{
+		rightCarsSpawned = 0;
+		leftCarsSpawned = 0;
+		rightCarsPassed = 0;
+		leftCarsPassed = 0;
+	}
+}
