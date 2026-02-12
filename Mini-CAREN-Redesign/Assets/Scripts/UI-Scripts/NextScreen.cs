e82d063136ccede2346b317c3982772d77f9f56a
drantino <65998488+drantino@users.noreply.github.com>
2026-02-12 14:34:52 -0700

    Navigation to other games though settings

--- Mini-CAREN-Redesign/Assets/Scripts/UI-Scripts/NextScreen.cs
+++ Mini-CAREN-Redesign/Assets/Scripts/UI-Scripts/NextScreen.cs
@@ -4,11 +4,33 @@ public class NextScreen : MonoBehaviour
 {
 
     // Thus script is used to load the next scene when a button is clicked.
-    
+
     public string NextScreenName;
 
     public void NextScreenButtonClicked()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(NextScreenName);
+        //Checks if there is a dedicated selection by name
+        if (!string.IsNullOrWhiteSpace(NextScreenName))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(NextScreenName);
+        }
+        else//if not goes to game index
+        {
+            switch (GameList.gameIndex)
+            {
+                case 0:
+                    {
+                        UnityEngine.SceneManagement.SceneManager.LoadScene("TargetTapGame");
+                        break;
+                    }
+                case 1:
+                    {
+                        UnityEngine.SceneManagement.SceneManager.LoadScene("JamesTestScene");
+                        break;
+                    }
+            }
+        }
+
+
     }
 }
