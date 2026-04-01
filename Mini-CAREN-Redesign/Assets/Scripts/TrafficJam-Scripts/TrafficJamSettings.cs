c206a53a01e52a638f3d7731480b2c7d72343afb
AaronVaartstra <aaron.vaartstra@gmail.com>
2026-04-01 12:46:31 -0600

    Fixed audio bug

--- Mini-CAREN-Redesign/Assets/Scripts/TrafficJam-Scripts/TrafficJamSettings.cs
+++ Mini-CAREN-Redesign/Assets/Scripts/TrafficJam-Scripts/TrafficJamSettings.cs
@@ -146,7 +146,7 @@ public class TrafficJamSettings : MonoBehaviour
             }
             else
             {
-                return carLength;
+                return masterVolume;
             }
         }
     }
